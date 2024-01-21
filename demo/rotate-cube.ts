e67e7a214893854651b6f246f970b3717623bab6
zhanglei_zl <zhanglei_zl@youzan.com>
2024-01-21 21:42:15 +0800

    feat: mock

--- demo/rotate-cube.ts
+++ demo/rotate-cube.ts
@@ -1,25 +1,26 @@
+// Replace the code in src/index.ts with the following code，then run 'pnpm run dev'
 import * as THREE from 'three';
 
-// 初始化 scene, camera, renderer
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
 const renderer = new THREE.WebGLRenderer();
-camera.position.z = 5;
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
 const geometry = new THREE.BoxGeometry( 1, 1, 1 );
 const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
 const cube = new THREE.Mesh( geometry, material );
+
 scene.add( cube );
 
-
+camera.position.z = 5;
 
 function animate() {
 	requestAnimationFrame( animate );
 
-	cube.rotation.z += 0.01;
-	// cube.rotation.y += 0.01;
+	cube.rotation.x += 0.01;
+	cube.rotation.y += 0.01;
 
 	renderer.render( scene, camera );
 }
